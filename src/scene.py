3d98e05fc4206c7920ef3e72b68eb9dd244d9c37
hakanalpp <hakana926@gmail.com>
2022-03-21 09:50:31 +0300

    abstract Shape class added

--- src/scene.py
+++ src/scene.py
@@ -1,16 +1,19 @@
-from math import sqrt
+# CENG 488 Assignment#1 by
+# Hakan Alp
+# StudentId: 250201056
+# March 2022
 
 from .camera import Camera
 from .math.vector import RGBA
 from .ray.ray import Ray
-from .shape.sphere import Sphere
+from .shape.shape import Shape
 
 
 class Scene:
-    def __init__(self, xRes, yRes, camera: 'Camera', objects: 'list[Sphere]', samples) -> 'Scene':
+    def __init__(self, xRes, yRes, camera: 'Camera', objects: 'list[Shape]', samples) -> 'Scene':
         self.resolution: 'tuple(int, int)'= (xRes, yRes)
         self.camera: 'Camera' = camera
-        self.objects: 'list[Sphere]' = objects
+        self.objects: 'list[Shape]' = objects
         self.samples = samples
         self.sentRayCount = 0
 
@@ -24,22 +27,8 @@ class Scene:
         inter = float('inf')
         color = RGBA(0,0,0,0)
         for obj in self.objects:
-            tempInt = self.intersect(ray, obj)
+            tempInt = obj.intersect(ray)
             if(tempInt != -1 and tempInt < inter):
                 inter = tempInt
                 color = obj.color
         return color
-    
-    def intersect(self, ray: 'Ray', obj: 'Sphere') -> 'float': # TODO
-        oc = ray.position - obj.position
-        a = ray.direction.dot(ray.direction)
-        b = 2.0 * oc.dot(ray.direction)
-        c = oc.dot(oc) - (obj.radius* obj.radius)
-        sqr_disc = b*b - 4*a*c
-        if sqr_disc < 0:
-            return -1
-        discriminant = sqrt(sqr_disc)
-        if (a == 0): a = 0.0000000001 # prevent division by zero
-        t0 = (-b + discriminant)/(2*a)
-        t1 = (-b - discriminant)/(2*a) 
-        return min(t0,t1)
